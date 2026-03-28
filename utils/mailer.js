98f0d1258fd1fceed60441d3307f2aef890ab24e
Virag23 <viragsjain1975@gmail.com>
2026-03-28 23:16:33 +0530

    First Commit

--- utils/mailer.js
+++ utils/mailer.js
@@ -7,6 +7,8 @@ function getTransporter() {
   return nodemailer.createTransport({
     service: 'gmail',
     auth: { user: process.env.EMAIL_USER, pass: process.env.EMAIL_PASS },
+    tls: { rejectUnauthorized: false },
+    family: 4,
   });
 }
 
